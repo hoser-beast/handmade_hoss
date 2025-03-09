7e2bf25c37cbf8b7f50e4f32ced866afda38861e
John Hosick <john@atikokancastle.com>
2025-03-09 00:22:57 -0600

    Day 003: Allocating a Backbuffer.

--- src/win32_handmade.cpp
+++ src/win32_handmade.cpp
@@ -1,30 +1,86 @@
 #include <windows.h>
 
+#define local_persist   static
+#define global_variable static
+#define internal        static
+
+// [TODO] Global variable for now.
+global_variable bool g_running;
+
+global_variable BITMAPINFO g_bitmap_info;
+global_variable void*      g_bitmap_memory;
+global_variable HBITMAP    g_bitmap_handle;
+global_variable HDC        g_bitmap_device_context;
+
+internal void
+win32_resize_dib_section(int width, int height)
+{
+    // [TODO] Bulletproof this.
+    // Maybe don't free first, free after, then free first if that fails.
+    if (g_bitmap_handle)
+    {
+        DeleteObject(g_bitmap_handle);
+    }
+
+    if (!g_bitmap_device_context)
+    {
+        // [TODO] Should we recreate these under certain special circumstances?
+        g_bitmap_device_context = CreateCompatibleDC(0);
+    }
+
+    g_bitmap_info.bmiHeader.biSize        = sizeof(g_bitmap_info.bmiHeader);
+    g_bitmap_info.bmiHeader.biWidth       = width;
+    g_bitmap_info.bmiHeader.biHeight      = height;
+    g_bitmap_info.bmiHeader.biPlanes      = 1;
+    g_bitmap_info.bmiHeader.biBitCount    = 32;
+    g_bitmap_info.bmiHeader.biCompression = BI_RGB;
+
+    // [TODO] Maybe we can just allocate this ourselves?
+    g_bitmap_handle = CreateDIBSection(g_bitmap_device_context, &g_bitmap_info,
+                                       DIB_RGB_COLORS, &g_bitmap_memory, 0, 0);
+}
+
+internal void
+win32_update_window(HDC device_context, int x, int y, int w, int h)
+{
+    StretchDIBits(device_context, x, y, w, h, x, y, w, h, g_bitmap_memory,
+                  &g_bitmap_info, DIB_RGB_COLORS, SRCCOPY);
+}
+
 LRESULT CALLBACK
-main_window_proc(HWND window, UINT message, WPARAM w_param, LPARAM l_param)
+win32_main_window_proc(HWND window, UINT message, WPARAM w_param,
+                       LPARAM l_param)
 {
     LRESULT result = 0;
 
     switch (message)
     {
+        case WM_ACTIVATEAPP:
+        {
+            OutputDebugStringA("WM_ACTIVATE_APP\n");
+            break;
+        }
         case WM_CLOSE:
         {
-            OutputDebugStringA("WM_CLOSE\n");
+            // [TODO] Handle this with a message to the user?
+            g_running = false;
             break;
         }
         case WM_DESTROY:
         {
-            OutputDebugStringA("WM_DESTROY\n");
+            // [TODO] Handle this as an error. Recreate window?
+            g_running = false;
             break;
         }
         case WM_SIZE:
         {
-            OutputDebugStringA("WM_SIZE\n");
-            break;
-        }
-        case WM_ACTIVATEAPP:
-        {
-            OutputDebugStringA("WM_ACTIVATE_APP\n");
+            RECT client_rect;
+            GetClientRect(window, &client_rect);
+
+            int w = client_rect.right - client_rect.left;
+            int h = client_rect.bottom - client_rect.top;
+
+            win32_resize_dib_section(w, h);
             break;
         }
         case WM_PAINT:
@@ -37,15 +93,7 @@ main_window_proc(HWND window, UINT message, WPARAM w_param, LPARAM l_param)
             int w = paint.rcPaint.right - paint.rcPaint.left;
             int h = paint.rcPaint.bottom - paint.rcPaint.top;
 
-            static DWORD operation = WHITENESS;
-
-            PatBlt(device_context, x, y, w, h, operation);
-
-            if (operation == WHITENESS)
-                operation = BLACKNESS;
-            else
-                operation = WHITENESS;
-
+            win32_update_window(device_context, x, y, w, h);
             EndPaint(window, &paint);
             break;
         }
@@ -60,42 +108,34 @@ main_window_proc(HWND window, UINT message, WPARAM w_param, LPARAM l_param)
 }
 
 int CALLBACK
-WinMain(HINSTANCE instance,
-        HINSTANCE prev_instance,
-        LPSTR     cmd_line,
-        int       cmd_show)
+WinMain(HINSTANCE instance, HINSTANCE prev_instance, LPSTR cmd_line,
+        int cmd_show)
 {
     WNDCLASS window_class = {};
 
     window_class.style         = CS_OWNDC | CS_HREDRAW | CS_VREDRAW;
-    window_class.lpfnWndProc   = main_window_proc;
+    window_class.lpfnWndProc   = win32_main_window_proc;
     window_class.hInstance     = instance;
     window_class.lpszClassName = "HandmadeHossWindowClass";
 
     if (RegisterClass(&window_class))
     {
-        HWND window_handle = CreateWindowEx(0,
-                                            window_class.lpszClassName,
-                                            "Handmade Hoss",
-                                            WS_OVERLAPPEDWINDOW | WS_VISIBLE,
-                                            CW_USEDEFAULT,
-                                            CW_USEDEFAULT,
-                                            CW_USEDEFAULT,
-                                            CW_USEDEFAULT,
-                                            0,
-                                            0,
-                                            instance,
-                                            0);
+        HWND window_handle
+            = CreateWindowEx(0, window_class.lpszClassName, "Handmade Hoss",
+                             WS_OVERLAPPEDWINDOW | WS_VISIBLE, CW_USEDEFAULT,
+                             CW_USEDEFAULT, CW_USEDEFAULT, CW_USEDEFAULT, 0, 0,
+                             instance, 0);
 
         if (window_handle)
         {
             MSG message;
 
-            for (;;)
+            g_running = true;
+            while (g_running)
             {
                 BOOL message_result = GetMessage(&message, 0, 0, 0);
 
-                if (message_result)
+                if (message_result > 0)
                 {
                     TranslateMessage(&message);
                     DispatchMessage(&message);
@@ -108,12 +148,12 @@ WinMain(HINSTANCE instance,
         }
         else
         {
-            // @TODO: Logging.
+            // [TODO] Logging.
         }
     }
     else
     {
-        // @TODO: Logging.
+        // [TODO] Logging.
     }
 
     return 0;
